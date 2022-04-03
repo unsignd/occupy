4abb3e793fb7bff24236f9c8626de8be1c8d6090
Yejun Kim <unsigndid@gmail.com>
2022-04-03 15:57:15 +0900

    ca

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require('https').createServer(
   {
     cert: fs.readFileSync(__dirname + '/cert/cert.pem'),
     key: fs.readFileSync(__dirname + '/cert/cert.key'),
+    ca: fs.readFileSync(__dirname + '/cert/ca.pem'),
     ciphers: [
       'ECDHE-RSA-AES128-SHA256',
       'DHE-RSA-AES128-SHA256',
